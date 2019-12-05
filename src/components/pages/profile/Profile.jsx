2ce7dc476cf05ab0d55a5f5ec581eb2e97521865
YevhenSoldatov1998 <arr.ess1998@gmail.com>
2019-12-05 13:42:44 +0300

    two commit

--- src/components/pages/profile/Profile.jsx
+++ src/components/pages/profile/Profile.jsx
@@ -3,7 +3,12 @@ class Profile extends React.Component{
     render(){
         return (
             <div className="profile">
-
+                <div>
+                    <div>back-img</div>
+                    <div>ava+descr</div>
+                </div>
+                <div>mypost</div>
+                <div>post</div>
             </div>
         )
     }
