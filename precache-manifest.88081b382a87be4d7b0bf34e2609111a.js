45da743a0c49f4e65afe391d4c5b15a59eaba72d
YevhenSoldatov1998 <arr.ess1998@gmail.com>
2020-02-08 11:43:24 +0200

    Updates

---
+++ precache-manifest.88081b382a87be4d7b0bf34e2609111a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31583cd054692fcedd1dff92ba4bd228",
+    "url": "/soc-network/index.html"
+  },
+  {
+    "revision": "c9dee78e6fa899a34535",
+    "url": "/soc-network/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "88f8848fc808c6858b05",
+    "url": "/soc-network/static/css/main.3f72ec51.chunk.css"
+  },
+  {
+    "revision": "c9dee78e6fa899a34535",
+    "url": "/soc-network/static/js/2.039ff4c6.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/soc-network/static/js/2.039ff4c6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "88f8848fc808c6858b05",
+    "url": "/soc-network/static/js/main.ca1640dd.chunk.js"
+  },
+  {
+    "revision": "ab1d0a6bda2eeeb72fa1",
+    "url": "/soc-network/static/js/runtime-main.40fdbebf.js"
+  }
+]);
