2a43ea778a42d05f3e5fe62110238c3180fac3fe
YevhenSoldatov1998 <arr.ess1998@gmail.com>
2020-02-10 19:55:42 +0300

    Acer simple commit

--- src/App.js
+++ src/App.js
@@ -8,31 +8,36 @@ import Setting from './components/pages/setting/Setting'
 import {Route, withRouter} from "react-router-dom";
 import NavbarContainer from "./components/navbar/NavbarContainer";
 import UsersContainer from "./components/pages/users/UsersContainer";
-import ProfileContainer from "./components/pages/profile/ProfileContainer";
 import {HeaderContainer} from "./components/header/HeaderContainer";
 import LoginReduxForm from "./components/pages/login/Login";
 import {connect} from "react-redux";
 import {compose} from "redux";
 import {InitializationApp} from "./redux/init-reducer";
 import Preloader from "./components/common/preloader";
+import { Suspense, lazy } from 'react';
 
+const ProfileContainer = lazy( () => import(`./components/pages/profile/ProfileContainer`));
 
 const App = (props) => {
     useEffect(() => {
         props.InitializationApp()
     }, []);
-
     if (!props.initialized) {
         return <Preloader/>
     }
-    console.log('APP')
     return (
         <main className="wrap-app">
             <HeaderContainer/>
             <NavbarContainer/>
 
             <article className="wrap-pages">
-                <Route path="/profile/:userId?"> <ProfileContainer/></Route>
+                <Route path="/profile/:userId?">
+                    <div>
+                        <Suspense fallback={<div>Loading...</div>}>
+                            <ProfileContainer />
+                        </Suspense>
+                    </div>
+                </Route>
                 <Route path="/messages"><Messages/></Route>
                 <Route path='/news'> <News/></Route>
                 <Route path="/music"> <Music/></Route>
